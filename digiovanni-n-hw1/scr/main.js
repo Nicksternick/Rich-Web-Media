0b304b794226a65ac7f09cef3a270da04c3963ae
Nicksternick <nicholasd1124@yahoo.com>
2024-02-07 08:41:49 -0500

    HW1 finished

--- digiovanni-n-hw1/scr/main.js
+++ digiovanni-n-hw1/scr/main.js
@@ -7,35 +7,54 @@ let words2;
 let words3;
 
 // Methods
-const LoadBabble = () =>
+
+// loadBabble(): use http requester to get the data
+const loadBabble = () =>
 {
+	// Link to the JSON info
 	const url = "data/babble-data.json";
+	// Create a new XMLHttpRequest
 	const xhr = new XMLHttpRequest();
+	// call babbleLoaded is the loading was a success
 	xhr.onload = babbleLoaded;
+	// Throw an errror if it fails to get the info
 	xhr.onerror = (e) => console.log(`Error - Code: ${e.target.responseText}`);
+
+	// Attemp to get the url
 	xhr.open("GET", url);
 	xhr.send();
 }
 
+// babbleLoaded(): setup the babble functions to work with the webpage
 const babbleLoaded = (e) =>
 {
+	// Set the text to the JSON info, and parse it
 	const text = e.target.responseText;
 	const json = JSON.parse(text);
 
+	// Fill each list with each array of words
 	words1 = json.words1;
 	words2 = json.words2;
 	words3 = json.words3;
 
+	// Generate one line of techno on startup
 	generateTechno(1);
+
+	// get each button and assign then the generateTechno() method with a number of techno to generate
 	document.querySelector("#babble-btn-1").addEventListener("click", () => generateTechno(1));
 	document.querySelector("#babble-btn-2").addEventListener("click", () => generateTechno(5));
 }
 
+// generatetechno(num): takes in a number and then generates that much techno babble 
 const generateTechno = (num) =>
 {
+	// Clear the innerHTML of #output so we can put new stuff in it
 	document.querySelector("#output").innerHTML = "";
+
+	// For each work that wants to be generated
 	for (let i = 0; i < num; i++)
 	{
+		//Add a new series of new techno babble
 		document.querySelector("#output").innerHTML += 
 		`<p>${randomElement(words1)}
 		${randomElement(words2)}
@@ -43,7 +62,6 @@ const generateTechno = (num) =>
 	}
 }
 
-window.onload = () => 
-{
-	LoadBabble();
-}
+// Call loadbabble 
+// when the page is loaded
+loadBabble();
--- digiovanni-n-hw1/scr/utils.js
+++ digiovanni-n-hw1/scr/utils.js
@@ -1,4 +1,6 @@
+// randomElement(array): Returns a random thing from the passed in array
 export const randomElement = (array) =>
 {
+	// Return a random thing within the array bounds
 	return array[(Math.random() * array.length) | 0]
 }
