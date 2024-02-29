229e57fa730940fac7bf07bd4c61e219bfa4c982
nsd3407 <nsd3407@MSS2150-19>
2024-02-29 18:42:14 -0500

    Please

--- digiovanni-n-hw2/src/audio.js
+++ digiovanni-n-hw2/src/audio.js
@@ -1,3 +1,34 @@
 // ===== | Variables | =====
+/**The Web Audio Context*/
+let audioCtx
 
-// ===== | Variables | =====
+/** the audio element instantiated in code */
+let audioElement
+
+/** The nodes used to analyize the audio data */
+let analyserNode;
+/** The nodes used to effect the audio sound */
+let gainNode;
+
+const DEFAULTS = Object.freeze({
+    gain: .5,
+    numSamples: 256
+});
+
+// ===== | Methods | =====
+
+/** 
+* Sets up the audio nodes to work on a new audio file
+* @param filePath the path to the audio file
+*/
+const setupWebAudio = (filePath) =>
+{
+    // Create a new audioContext, and have a fallback incase window.AudioContex is not supported
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+    // Fill the audioCtx with this new context
+    audioCtx = AudioContext();
+
+    // fill the audio element with a new audio tag
+    audioElement = new Audio();
+}
