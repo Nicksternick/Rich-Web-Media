e3f60e06e58fdca99d08e86db39a52b0722279b8
Nicksternick <111940777+Nicksternick@users.noreply.github.com>
2024-04-11 10:42:46 -0400

    RAH

---
+++ threeJS/src/main.js
@@ -0,0 +1,23 @@
+import * as THREE from 'three';
+
+// Set up the scene
+const scene = new THREE.Scene();
+
+// Set up the camera
+const camera = new THREE.PerspectiveCamera(75, 
+window.innerWidth /window.innerHeight, 0.1, 1000);
+
+// Set up the renderer
+const renderer = new THREE.WebGLRenderer();
+// Set it's size
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Append the renderer to the document
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube);
+
+camera.position.z = 5;
